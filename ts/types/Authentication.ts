ab52f7418ae0b1a804ee21ac96f78a9978b740a0
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-05-24 17:23:45 +0700

    Remote Data Auth

---
+++ ts/types/Authentication.ts
@@ -0,0 +1,25 @@
+import { RemoteResponse } from './RemoteResponse';
+
+type LoginSuccessResponseData = {
+  message: string;
+  email: string;
+  token: string;
+}
+
+type RegisterErrorResponseMessageField = {
+  username?: string;
+  email?: string;
+  password?: string;
+};
+
+type RegisterErrorResponseMessage = RemoteResponse & {
+  message: RegisterErrorResponseMessageField;
+};
+
+export type LoginSuccessResponse = RemoteResponse & {
+  data: LoginSuccessResponseData;
+};
+
+export type RegisterErrorResponse = RemoteResponse & {
+  data: RegisterErrorResponseMessage;
+};
