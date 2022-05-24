040bb28aa77ca6a1a6b9c6c5737ee1164c39e9e7
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-05-24 17:28:20 +0700

    Delete folder

--- types/NextPageWithLayout.ts
+++
@@ -1,7 +0,0 @@
-import type { ReactElement, ReactNode } from 'react';
-
-import type { NextPage } from 'next';
-
-export type NextPageWithLayout<T = {}> = NextPage<T> & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
