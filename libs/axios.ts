a12ae5b00caa209738f3a896d52929c52bdc999f
Deva Aji Saputra <80871396+Devaaji@users.noreply.github.com>
2022-05-28 10:05:47 +0700

    Change http

--- libs/axios.ts
+++ libs/axios.ts
@@ -6,7 +6,7 @@ import { parseCookies } from 'nookies';
 import useUserStore from '../store/useUserStore';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/api/',
+    baseURL: 'https://api-ecommerce-depdep.herokuapp.com/api/',
 });
 
 axiosInstance.interceptors.request.use((config) => {
